3c6532ffaf5597c7f2d7236e0d0e0a71d39ad9ee
LINUSOBURA <linusobura@gmail.com>
2024-07-24 15:37:01 +0300

    4 MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" MRUCache module """
+
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+	MRUCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Adds an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            f_key, _ = self.cache_data.popitem(last=True)
+            print("DISCARD: {}".format(f_key))
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
