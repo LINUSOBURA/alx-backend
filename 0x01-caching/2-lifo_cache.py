ac73799db0944705db597cda70e08de8cb96a02e
LINUSOBURA <linusobura@gmail.com>
2024-07-24 14:34:24 +0300

    fixed put

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ FIFOCache module """
 
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
@@ -9,19 +11,23 @@ class LIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
         Adds an item to the cache.
         """
-        if key is not None and item is not None:
 
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                self.cache_data.pop(last_key)
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD: {}".format(last_key))
 
-            self.cache_data[key] = item
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """
