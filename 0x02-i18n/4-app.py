2680fb5e6dddf908a36621a3ceea03d05d1776a8
LINUSOBURA <linusobura@gmail.com>
2024-07-30 23:14:12 +0300

    locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,15 +20,18 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """get locale"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index():
     """ Default route"""
     home_title = gettext('home_title')
     home_header = gettext('home_header')
-    return render_template('3-index.html',
+    return render_template('4-index.html',
                            home_title=home_title,
                            home_header=home_header)
 
